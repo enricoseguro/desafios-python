fd305b051b1531b045d123ca0aba76000d911228
Enrico Seguro <enricoseguropb@gmail.com>
2025-06-19 02:35:26 -0300

    novos desafios

---
+++ d024.py
@@ -0,0 +1,6 @@
+"""
+Crie mum programa que leia o nome de uma didade e diga se ela começa ou não com o nome "SANTO".
+"""
+cidade = input('Digite o nome de uma cidade: ')
+cidade = cidade.upper()
+print('SANTO' in cidade)
