5bdf320d4c65c280757d18172a51173d10707f73
Enrico Seguro <enricoseguropb@gmail.com>
2025-06-20 20:30:57 -0300

    Update d034.py

--- d034.py
+++ d034.py
@@ -1,7 +1,12 @@
 salario = float(input('Digite o seu salário: '))
+cores = {'limpa': '\033[m',
+         'vermelho': '\033[1;31m',
+         'verde': '\033[1;32m', }
 if salario > 1250.00:
     salario = salario + (salario * 0.10)
-    print(f'Seu salário irá aumentar 10%, ficando R${salario:.2f}')
+    print(
+        f'Seu salário irá aumentar {cores['vermelho']}10%{cores['limpa']}, ficando {cores['vermelho']}R${salario:.2f}{cores['limpa']}.')
 else:
     salario = salario + (salario * 0.15)
-    print(f'Seu salário irá aumentar 15%, ficando R${salario:.2f}')
+    print(
+        f'Seu salário irá aumentar {cores['verde']}15%{cores['limpa']}, ficando {cores['verde']}R${salario:.2f}{cores['limpa']}.')
